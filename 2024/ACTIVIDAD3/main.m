90e847182d12a02d2a9fb8f795923f11bfa576eb
vasanza <vasanza@gmail.com>
2024-06-22 17:08:50 +0200

    Add files via upload

---
+++ 2024/ACTIVIDAD3/main.m
@@ -0,0 +1,33 @@
+% Actividad #3
+% Objetivo: Graficar con subplot y guardarlos
+% Nombre: sunombre
+
+% Limpiar variables y linea de comandos
+clear%para borrar el workspace y liberar memoria RAM
+clc %limpiar el command window
+
+%% Generar datos alatorios
+% En todo dataset las filas son muestras y las comlumnas son variables
+variables=5; %Columnas
+vname=["Corriente","Voltaje","variable3","variable4","variable5"];
+muestras=1000; %Filas
+Dataset=rand(muestras, variables);
+%matriz de muestras (filas) x variables (columnas)
+
+%% Graficar el dataset
+figure %no sobrescribe la anterior grafica
+
+subplot(1,2,1)
+plot(Dataset(:,2:3));%
+title("Datos aleatorios");
+xlabel("muestras");
+ylabel("valor");
+legend(vname(2:3))
+
+subplot(1,2,2)
+plot(Dataset(:,[1,4,5]));
+title("Datos aleatorios");
+xlabel("muestras");
+ylabel("valor");
+legend(vname([1,4,5]))
+%clear
